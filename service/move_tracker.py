735966028ce479298a0a9e844d03527233bd4dbb
mjh <majh118@chinaunicom.cn>
2024-12-03 10:31:57 +0800

    线程通知唤醒机制优化CPU占用

--- service/move_tracker.py
+++ service/move_tracker.py
@@ -15,6 +15,7 @@ class MoveTracker(tk.BooleanVar):
         self.width_value = width_value
         # 线段缓存，用于存储鼠标移动的线段，在图像上绘制
         self.line_cache = deque()
+        self.condition = threading.Condition()
 
         # 开启新线程
         self.render_timer = threading.Thread(target=self.render).start()
@@ -28,22 +29,31 @@ class MoveTracker(tk.BooleanVar):
         """渲染，清空缓存，将线段缓存绘制到图像上"""
         # 线程一旦开启就不再退出，等待主线程关闭
         while True:
-            # 一旦队列中有元素，则渲染，直到队列只有一个点，表示鼠标不动
-            while len(self.line_cache) > 1:
-                start = self.line_cache.popleft()
-                end = self.line_cache[0]
-                self.cache.line(
-                    start=start,
-                    end=end,
-                    color=(255, 255, 255, self.alpha_value.get()),
-                    width=self.width_value.get()
-                )
+            # 等待条件变量，直到队列中有新的元素
+            with self.condition:
+                while len(self.line_cache) <= 1:
+                    self.condition.wait()  # 等待直到有新的点加入到队列中
+
+                # 处理队列中的线段
+                while len(self.line_cache) > 1:
+                    start = self.line_cache.popleft()
+                    end = self.line_cache[0]
+                    self.cache.line(
+                        start=start,
+                        end=end,
+                        color=(255, 255, 255, self.alpha_value.get()),
+                        width=self.width_value.get()
+                    )
+
+            # time.sleep(0.01)  # 限制渲染线程的CPU占用
 
     def on_move(self, x: int, y: int):
         """鼠标移动监听事件函数"""
         if self.get():
-            # 此处将在每次鼠标移动时调用，非常频繁，因此需要优化
-            # 如果该点已经在缓存中最后一个元素，则不再重复添加
             if self.line_cache and self.line_cache[-1] == (x, y):
                 return
             self.line_cache.append((x, y))
+
+            # 通知渲染线程队列有新数据
+            with self.condition:
+                self.condition.notify()  # 通知渲染线程可以进行渲染
